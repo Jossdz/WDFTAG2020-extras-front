f832168e97027e642582eb675aa65149c0608559
jossdz <carloscorrea.dev@gmail.com>
2020-09-23 12:57:14 -0500

    infinite scroll explained

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,25 @@
 import React from "react"
 import { Card, CardHeader, CardBody, Avatar, Button, Box } from "grommet"
+// importamos el hook useSRWInfinite para pedir la data y crear el infitescroll
 import { useSWRInfinite } from "swr"
 
+//SWR requiere una funcion con la cual hara la peticion al recurso
 const fetcher = url => fetch(url).then(r => r.json())
 
 function Home() {
+  // Extraemos del hook la data que es la informaci'on que va extrayendo del recurso
+  // size para saber cuantos elementos debenos traer
+  // y Set size que nos permite traer la siguiente pagina de nuestro recurso
   const { data, size, setSize } = useSWRInfinite((index, previousPageData) => {
+    // el hook internamente devuelve null si la longitud de la pagina anterior es 0
     if (previousPageData && previousPageData.length === 0) return null
+    // y devolvemos la ruta a la cual vamos a pedir la informacion paginada con nuestro index+1
+    // asi cada vez que incremente la pagina con setSize pediremos los datos siguientes
     return `https://rickandmortyapi.com/api/character/?page=${index + 1}`
   }, fetcher)
+  // Esta funcion fue requerida gracias a que la api de R&M nos devuelve un objeto con los resultados
   const chars = data ? data.map(({ results }) => results) : []
+  // Y como chars es un arreglo de arreglos con los resultados de cada pagina necesitamos hacer flat para que sea solo un arreglo
   console.log("CHARs", chars.flat())
   return !data ? (
     <p>Loading...</p>
